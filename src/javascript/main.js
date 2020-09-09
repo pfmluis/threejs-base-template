be8f092f359708d01a59ff9cdf6d46484ac3de75
pedroluis <pedro.luis@edirectinsure.com>
2020-09-09 20:58:00 +0200

    Created basic template to start a three.js project

---
+++ src/javascript/main.js
@@ -0,0 +1,24 @@
+import * as THREE from '../../assets/three.module';
+
+(() => {
+  const canvas = document.querySelector('#canvas');
+  const renderer = new THREE.WebGL1Renderer({canvas});
+  const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 5);
+  const scene = new THREE.Scene();
+
+  const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+  const cubeMaterial = new THREE.MeshBasicMaterial({color: 0xaa0000});
+  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+  camera.position.z = 2;
+  scene.add(cube);
+
+  const animate = () => {
+    cube.rotation.y += 0.02;
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(animate);
+  };
+
+  animate();
+})();
