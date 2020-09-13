c26a82fcbf8ceee1b99a94f9642dfac80fabe140
pedroluis <pedro.luis@edirectinsure.com>
2020-09-13 14:29:11 +0200

    Added three package

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,15 +1,15 @@
-import * as THREE from '../../assets/three.module';
 import { updateCameraAspect } from './utils/camera.utils';
+import { WebGL1Renderer, PerspectiveCamera, Scene, BoxGeometry, MeshBasicMaterial, Mesh } from 'three';
 
 (() => {
   const canvas = document.querySelector('#canvas');
-  const renderer = new THREE.WebGL1Renderer({canvas});
-  const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 5);
-  const scene = new THREE.Scene();
+  const renderer = new WebGL1Renderer({canvas});
+  const camera = new PerspectiveCamera(75, 2, 0.1, 5);
+  const scene = new Scene();
 
-  const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-  const cubeMaterial = new THREE.MeshBasicMaterial({color: 0xaa0000});
-  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  const cubeGeometry = new BoxGeometry(1, 1, 1);
+  const cubeMaterial = new MeshBasicMaterial({color: 0xaa0000});
+  const cube = new Mesh(cubeGeometry, cubeMaterial);
 
   camera.position.z = 2;
   scene.add(cube);
